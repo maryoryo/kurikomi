83dc4f3d94ad5d5e8e0e299f0fb1cacc16f18e5d
maryoryo <liyomaxxde@gmail.com>
2021-07-15 08:01:26 +0000

    deploy setting

--- app/controllers/public/group_post_comments_controller.rb
+++ app/controllers/public/group_post_comments_controller.rb
@@ -6,16 +6,16 @@ class Public::GroupPostCommentsController < ApplicationController
     @group_post_comment = GroupPostComment.new(group_post_comment_params)
     @group_post_comment.group_post_id = @group_post.id
     @group_post_comment.user_id = current_user.id
-    byebug
-    if @group_post_comment.save
-      redirect_back(fallback_location: root_path)
-    else
-      redirect_back(fallback_location: root_path)
-    end
-    # unless @group_post_comment.save
-    #   render "group_posts/show"
-    # end
-  end
+    
+  #   if @group_post_comment.save
+  #     redirect_back(fallback_location: root_path)
+  #   else
+  #     redirect_back(fallback_location: root_path)
+  #   end
+  #   # unless @group_post_comment.save
+  #   #   render "group_posts/show"
+  #   # end
+  # end
   
   def destroy
     @group_post = GroupPost.find(params[:id])
