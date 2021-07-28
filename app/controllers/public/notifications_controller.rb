17e9df43a6e05e24edd4182e74429d8d3b808d6e
maryoryo <liyomaxxde@gmail.com>
2021-07-28 15:33:52 +0000

    [Fix]20210728

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -5,13 +5,5 @@ class Public::NotificationsController < ApplicationController
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
     end
-    # @notification = Notification.find_by(params[:visited_id]) 1
-    # @group = Group.find([:group_id])
-    # @group_post = @notification.group_post 1
-    # @group = Group.find(params[:group_id])
-    # @group_post = GroupPost.find(params[:group_id])
-    # @group = Group.find()
-    # @group_post = @group_post.group_post
-    # @group = @group_post.group 1
   end
 end
