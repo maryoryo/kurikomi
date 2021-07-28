aa1162d79e472ca9cb759a70b87ae745aafe432d
maryoryo <liyomaxxde@gmail.com>
2021-07-28 16:44:22 +0000

    [complete]20210728

--- app/controllers/public/users/passwords_controller.rb
+++ app/controllers/public/users/passwords_controller.rb
@@ -31,6 +31,4 @@ class Public::Users::PasswordsController < Devise::PasswordsController
   # def after_sending_reset_password_instructions_path_for(resource_name)
   #   super(resource_name)
   # end
-  
-  validates :password, length: { minimum: 6 }
 end
