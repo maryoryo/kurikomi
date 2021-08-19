24a3b903c77442f4c4fee4328113f11b50481a18
maryoryo <liyomaxxde@gmail.com>
2021-08-19 10:35:14 +0000

    [bug logger]

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -2,6 +2,7 @@ class InquiryMailer < ApplicationMailer
 
   def send_mail(inquiry)
     @inquiry = inquiry
+    Rails.logger.debug(ENV['TOMAIL'])
     mail to: ENV['TOMAIL'], subject: '【お問い合わせ】' + @inquiry.subject
   end
 end
